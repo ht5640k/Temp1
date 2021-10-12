575fe24f2e5ea4fc01e4340ecf4328abe40bbb96
ht5640k <ht5640k@gre.ac.uk>
2021-10-12 17:21:37 +0100

    <feat> no returns system cpu usage stats

--- systemStats.sh
+++ systemStats.sh
@@ -5,8 +5,8 @@
 
 # this script is designed to gather system statistics
 
-AUTHOR= "HT"
-VERSION= "1"
+AUTHOR="HT"
+VERSION="2"
 RELEASED="2021-10-05"
 
 # DISPLAY  HELP MESSAGE
@@ -14,20 +14,20 @@ RELEASED="2021-10-05"
 
  USAGE() {
      echo -e $1
-     echo -e "\nUsage: systemStas [ - t temperature] [ -i ipv4 address]"
+     echo -e "\nUsage: systemStas [ - t temperature] [ -i ipv4 address] [-c cpu usage]"
      echo -e "\t\t  [ -v version]"
      echo -e "\t\t more information see man systemStats"
 
 }
 
 # check for arguments (error checking)
-if [ $# -lt 1]; then
+if [ $# -lt 1 ]; then
            USAGE  " Not enough arguments"
            exit 1
 elif [ $# -gt 3 ]; then
            USAGE  " Too many arguments supplie"
            exit 1
-elif [[ ( $1 == '-h')  || ( $1 == ' --help' ) ]]; then
+elif [[ ( $1 == '- h' )  || ( $1 == ' --help' ) ]]; then
            USAGE  "Help!"
            exit 1
 
@@ -37,8 +37,21 @@ fi
 # With the flags method, some of the arguments can be made mandatory or optional
 # a:b  (a is mandatory, b is optional) abc is all optional
 
-while getopts tiv OPTION
+while getopts ctiv OPTION
 do 
 case  ${OPTION}
 in
+i) IP=$(ifconfig waln0 | grep -W inet | awk '{print$2}')
+   echo ${IP};;
+c) USAGE=$( grep -w 'cpu' /proc/stat | awk '{usage=($2+$3+$4+$5+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)}
+                                              {free=($5)*100/($2+$3+$4+$5+$6+$7+$8)}
+                                               END {printf "Used CPU: %.2f%%\n",usage}
+                                                   {printf "Free CPU: %.2f%%\n",free}' )
+    echo -e ${USAGE};;
+t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
+   echo ${TEMP} "need to divide by thousand...";;
+v) echo -e "systemStats:\n\t   Version: ${version} Released: ${RELEASED} Author: ${AUTHOR}";;
+esac
+done
 
+# end of script
