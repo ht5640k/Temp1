642500c4a1fc3872ca3ce53ed752112330212211
ht5640k <ht5640k@gre.ac.uk>
2021-10-19 16:56:47 +0100

    <feat> now outputs to log file#

--- systemStats.sh
+++ systemStats.sh
@@ -8,6 +8,7 @@
 AUTHOR="HT"
 VERSION="2"
 RELEASED="2021-10-05"
+FILE=~/ACE/systemStats.log 
 
 # DISPLAY  HELP MESSAGE
  
@@ -41,17 +42,20 @@ while getopts ctiv OPTION
 do 
 case  ${OPTION}
 in
-i) IP=$(ifconfig waln0 | grep -W inet | awk '{print$2}')
+i) IP=$(ifconfig wlan0 | grep -w inet | awk '{print$2}')
    echo ${IP};;
 c) USAGE=$( grep -w 'cpu' /proc/stat | awk '{usage=($2+$3+$4+$5+$6+$7+$8)*100/($2+$3+$4+$5+$6+$7+$8)}
                                               {free=($5)*100/($2+$3+$4+$5+$6+$7+$8)}
-                                               END {printf "Used CPU: %.2f%%\n",usage}
-                                                   {printf "Free CPU: %.2f%%\n",free}' )
+                                               END {printf "Used CPU: %.2f%%",usage}
+                                                   {printf " Free CPU: %.2f%%",free}' )
     echo -e ${USAGE};;
 t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
    echo ${TEMP} "need to divide by thousand...";;
 v) echo -e "systemStats:\n\t   Version: ${version} Released: ${RELEASED} Author: ${AUTHOR}";;
 esac
 done
+
+ NOW=$(date +%Y-%m-%dT%H:%M:%SZ)
+        echo -e " ${NOW}\tIP:${IP} Temperature: ${TEMP} CPU: ${USAGE}" >> ${FILE}
 
 # end of script
